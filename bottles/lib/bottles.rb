d0dfb80e5e31599e563d863eadabd72608da711d
Yago Santos <yagosansz@gmail.com>
2025-08-30 11:40:36 -0300

    feat: 2nd try

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,48 +1,48 @@
 class Bottles
-      def song
-        verses(99, 0)
-      end
+  def song
+    verses(99, 0)
+  end
 
-      def verses(upper, lower)
-        upper.downto(lower).map { |i| verse(i) }.join("\n")
-      end
+  def verses(upper, lower)
+    upper.downto(lower).map { |i| verse(i) }.join("\n")
+  end
 
-      def verse(number)
-        case number
-        when 0
-          "No more bottles of beer on the wall, no more bottles of beer.\n" \
-            "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-        else
-          "#{number} #{container(number)} of beer on the wall, " \
-            "#{number} #{container(number)} of beer.\n" \
-            "Take #{noun(number)} down and pass it around, #{remainder(number)} #{container(number-1)} of beer on the wall.\n"
-        end
-      end
-
-      # TODO: quantity
-      def remainder(number=:FIXME)
-        if number == 1
-          "no more"
-        else
-          number - 1
-        end        
-      end
-
-      def container(number)
-        if number == 1
-          "bottle"
-        else
-          "bottles"
-        end
-      end
-
-      # TODO: pronoun
-      def noun(number)
-        if number == 1
-          "it"
-        else
-          "one"
-        end
-      end
-
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    else
+      "#{number} #{container(number)} of beer on the wall, " + 
+      "#{number} #{container(number)} of beer.\n" +
+      "Take #{pronoun(number)} down and pass it around, " +
+      "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end    
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number
+    end
+  end
+end
