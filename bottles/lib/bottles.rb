3f96ea1a7632388b8af209e668494a9ea3f51c2a
Yago Santos <yagosansz@gmail.com>
2025-08-30 11:06:49 -0300

    feat: first try on Dry'ing up and finding more abstractions

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,20 +10,39 @@ class Bottles
       def verse(number)
         case number
         when 0
-          "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-        when 1
-          "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+          "No more bottles of beer on the wall, no more bottles of beer.\n" \
+            "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
         else
-          "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+          "#{number} #{container(number)} of beer on the wall, " \
+            "#{number} #{container(number)} of beer.\n" \
+            "Take #{noun(number)} down and pass it around, #{remainder(number)} #{container(number-1)} of beer on the wall.\n"
         end
+      end
+
+      # TODO: quantity
+      def remainder(number=:FIXME)
+        if number == 1
+          "no more"
+        else
+          number - 1
+        end        
       end
 
       def container(number)
         if number == 1
           "bottle"
-         else
+        else
           "bottles"
-         end
+        end
+      end
+
+      # TODO: pronoun
+      def noun(number)
+        if number == 1
+          "it"
+        else
+          "one"
+        end
       end
 
     end
