aaee066823db664e19681e726a4170fc18d7f6c2
Yago Santos <yagosansz@gmail.com>
2025-09-27 11:29:29 -0300

    fix: safely remove type check

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,13 +9,11 @@ class Bottles
 
   def verse(number)
     bottle_number = BottleNumber.for(number)
-    next_bottle_number = BottleNumber.for(bottle_number.successor)
-    # next_bottle_number = bottle_number.successor
 
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number} of beer on the wall.\n"
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
@@ -27,7 +25,7 @@ class BottleNumber
   end
 
   def self.for(number)
-    return number if number.kind_of?(BottleNumber)
+    # return number if number.kind_of?(BottleNumber)
 
     case number
     when 0
@@ -60,7 +58,7 @@ class BottleNumber
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number - 1)
   end
 end
 
@@ -74,7 +72,7 @@ class BottleNumber0 < BottleNumber
   end
 
   def successor
-    99
+    BottleNumber.for(99)
   end
 end
 
