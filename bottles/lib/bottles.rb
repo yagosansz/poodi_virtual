131feb5d95eb60e7366aa4e83ba93399f4e15f6d
Yago Santos <yagosansz@gmail.com>
2025-10-06 11:36:28 -0300

    refactor: factory candidates can now self-register to it!

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -20,10 +20,18 @@ end
 
 class BottleNumber
   def self.for(number)
-    [BottleNumber1, BottleNumber0, BottleNumber].
-      find { |candidate| candidate.handles?(number) }.
-      new(number)    
+    registry.find { |candidate| candidate.handles?(number) }.new(number) 
   end
+
+  def self.registry
+    @registry ||= []
+  end
+
+  def self.register(candidate)
+    registry.prepend(candidate)
+  end
+
+  BottleNumber.register(self)
 
   def self.handles?(number)
     true
@@ -60,6 +68,8 @@ class BottleNumber
 end
 
 class BottleNumber0 < BottleNumber
+  BottleNumber.register(self)
+
   def self.handles?(number)
     number == 0
   end
@@ -78,6 +88,8 @@ class BottleNumber0 < BottleNumber
 end
 
 class BottleNumber1 < BottleNumber
+  BottleNumber.register(self)
+
   def self.handles?(number)
     number == 1
   end
