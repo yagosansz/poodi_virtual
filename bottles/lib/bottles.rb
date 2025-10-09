f3adb3cef19a4e1172d70f459774df5ef1214e64
Yago Santos <yagosansz@gmail.com>
2025-10-09 10:53:24 -0300

    docs: pseudocoding new requirement to introduce code smell

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -8,12 +8,18 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.for(number)
+    # if 99_bottles_song? 
+      bottle_number = BottleNumber.for(number)
 
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
+      "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" +
+      "#{bottle_number.action}, " +
+      "#{bottle_number.successor} of beer on the wall.\n"
+    # elsif verse_for_song_2?
+    #   "verse 2"
+    # elsif verse_for_song_3?
+    #   "verse 3"
+    # end
   end
 end
 
