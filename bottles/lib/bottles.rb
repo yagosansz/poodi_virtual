3b57afc48aaa16884904420450247645a577d5be
Yago Santos <yagosansz@gmail.com>
2025-10-13 21:07:30 -0300

    refactor: push object creation to the edge!

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -20,18 +20,16 @@ end
 
 class BottleVerse
   def self.lyrics(number)
-    new(number).lyrics
+    new(BottleNumber.for(number)).lyrics
   end
 
-  attr_reader :number
+  attr_reader :bottle_number
 
-  def initialize(number)
-    @number = number
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
   end
 
   def lyrics
-    bottle_number = BottleNumber.for(number)
-
     "#{bottle_number} of beer on the wall, ".capitalize +
     "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
