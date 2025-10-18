a3d24306309e41e21b872d4e80b42fc725b3383b
Yago Santos <yagosansz@gmail.com>
2025-10-18 11:24:19 -0300

    test: write intention revealing tests
    Context specific tests (Bottles) doesn't make much more sense!

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -43,38 +43,34 @@ class BottleVerseTest < Minitest::Test
   end  
 end
 
+class VerseFake
+  def self.lyrics(number)
+    "This is verse #{number}.\n"
+  end
+end
+
 class CountdownSongTest < Minitest::Test
-  def test_a_couple_verses
-    expected = "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(99, 98)
+  def test_verses
+    expected = "This is verse 500.\n"
+    assert_equal(
+      expected,
+      CountdownSong.new(verse_template: VerseFake).verse(500)
+    )
   end
 
-  def test_a_few_verses
-    expected = "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(2, 0)
+  def test_verses
+    expected = 
+      "This is verse 99.\n" +
+        "\n" +
+        "This is verse 98.\n" +
+        "\n" +
+        "This is verse 97.\n" 
+    assert_equal(
+      expected, 
+      CountdownSong.new(verse_template: VerseFake).verses(99, 97)
+    )
   end
-
+  
   def test_the_whole_song
     expected = <<~SONG
       99 bottles of beer on the wall, 99 bottles of beer.
